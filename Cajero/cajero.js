03544388c99a3e9e08ca9a14bfce8188fac6cbe3
ateniki <atenasgama@gmail.com>
2022-02-15 18:01:25 -0600

    proyecto cajero y actualización de practicas

---
+++ Cajero/cajero.js
@@ -0,0 +1,23 @@
+function login1(form){
+    document.getElementById().innerHTML += value
+    if(form.us.value=="Mali"){
+        if(form.co.value=="123"){
+            location="principal.html"
+        }else{
+            alert("Contraseña errónea")
+        }else{
+            alert("Usuario erróneo")
+        }
+    }
+}
+
+function formula1(){
+    var total = parseInt(document.getElementById("cant").value)
+    var total = parseInt(document.getElementById("s1").value)
+    document.getElementById(s1).value =(Saldo+total)
+}
+function formula2(){
+    var total = parseInt(document.getElementById("cant").value)
+    var total = parseInt(document.getElementById("s1").value)
+    document.getElementById(s1).value =(Saldo-total)
+}
